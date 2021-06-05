d8850ff8011b3ce8b5777821ca0b60eeb6279844
Jaivant <59435891+JAIVANT30@users.noreply.github.com>
2021-06-05 12:52:32 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <span style={{ color: "black" }}>Z</span>ukaki
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
